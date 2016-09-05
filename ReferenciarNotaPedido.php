b6d340f2d2ce09657f56c114b0d38f19a5c11d49
FacTrónica <webmaster@factronica.cl>
2016-09-05 00:31:25 -0300

    Create ReferenciarNotaPedido.php

---
+++ ReferenciarNotaPedido.php
@@ -0,0 +1,25 @@
+<?php
+
+##################################################################                                                
+#######    REFERENCIAS DE FACTURACIÓN CONTRA NOTA DE PEDIDO/VENTA                                                                              
+##################################################################
+#
+# Debe ir vacio
+$doc_referencia["1"]["IndGlobal"]="";    
+#
+# Folio de la Nota de Pedido o Nota de venta
+$doc_referencia["1"]["FolioRef"]="3345";                                                  
+#
+# 802 es el codigo para notas de pedido/venta
+$doc_referencia["1"]["TpoDocRef"]="802"; 
+#
+# Comentario de la Referencia
+$doc_referencia["1"]["RazonRef"]="Nota de Pedido 3345";                                                           
+#
+# El codigo de referencia debe ir vacio
+$doc_referencia["1"]["CodRef"]="";
+#
+# Fecha de de emisión de la nota de pedido formato aaaa-mm-dd
+$doc_referencia["1"]["FchRef"]="2016-04-28"; 
+
+?>
